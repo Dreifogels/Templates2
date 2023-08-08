4887f595ce2117e8fa305efd37264489fcb9c4b4
Gints <gintsdreifogels@gmail.com>
2023-08-08 13:28:53 +0300

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName('myImgNew');
+var img = document.getElementsByClassName('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
